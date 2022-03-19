9b7a476d6624689111d6933f3f94f46ef1f8e3ef
Natan Kayevo <kayevo@protonmail.com>
2022-03-19 20:31:40 -0300

    Feature: add diffucult level game phase

--- guess_number/lib/guess_number.ex
+++ guess_number/lib/guess_number.ex
@@ -7,6 +7,18 @@ defmodule GuessNumber do
   end
 
   def run() do
-    IO.puts("Welcome do the Guess The Number Game")
+    IO.puts("Welcome do the Guess The Number Game.")
+
+    IO.gets("Pick the game difficult level (1, 2 or 3): ")
+    |> Integer.parse()
+    |> validateLevel()
+    |> IO.inspect()
   end
+
+  def validateLevel(:error) do
+    IO.puts("Invalid level.")
+    run()
+  end
+
+  def validateLevel({intLevel, _}), do: intLevel
 end
