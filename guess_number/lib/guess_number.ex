faa20e3c69a533570e958743225535c9f313c056
Natan Kayevo <kayevo@protonmail.com>
2022-03-22 20:56:40 -0300

    Feature: generate number on range according level.

--- guess_number/lib/guess_number.ex
+++ guess_number/lib/guess_number.ex
@@ -12,6 +12,8 @@ defmodule GuessNumber do
     IO.gets("Pick the game difficult level (1, 2 or 3): ")
     |> Integer.parse()
     |> validateLevel()
+    |> get_range()
+    |> Enum.random()
     |> IO.inspect()
   end
 
@@ -21,4 +23,23 @@ defmodule GuessNumber do
   end
 
   def validateLevel({intLevel, _}), do: intLevel
+
+  def get_range(level) do
+    case level do
+      1 ->
+        1..10
+
+      2 ->
+        1..100
+
+      3 ->
+        1..1000
+
+      _ ->
+        IO.puts("Invalid level.")
+        run()
+    end
+  end
+
+
 end
